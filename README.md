1cbda1632c147c99b5d63de3bca7041c9b4dab52
baksale <java.rabota@gmail.com>
2020-02-21 21:12:24 +0000

    completed api

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # dx-package-api
 API to work with salesforce dx packages
+
+## Low level api
+Check [api.ts](./src/main/api.ts)
+
+## Caching api
+Check [cachingapi.ts](./src/main/cachingapi.ts)
---
+++ src/main/cachingapi.ts
@@ -0,0 +1,39 @@
+import { DxPackageMetadataApi } from "./api";
+import { Package2Version } from "./model";
+
+export interface DxPackageMetadataCachingApi {
+  getPackage2VersionById(subscriberPackageVersionId: string): Promise<Package2Version>;
+  getDependencies(subscriberPackageVersionId: string): Promise<Package2Version[]>;
+}
+
+export class DxPackageMetadataCachingApiImpl implements DxPackageMetadataCachingApi {
+  private packageVersions: Map<string, Package2Version> = new Map();
+  private packageVersionIdToDependenciesIds: Map<string, string[]> = new Map();
+
+  constructor(private dxPackageMetadataApi: DxPackageMetadataApi) {}
+
+  public async getPackage2VersionById(subscriberPackageVersionId: string): Promise<Package2Version> {
+    if (!this.packageVersions.has(subscriberPackageVersionId)) 
+      this.packageVersions.set(subscriberPackageVersionId, await this.dxPackageMetadataApi.getPackage2VersionById(subscriberPackageVersionId));
+    return this.packageVersions.get(subscriberPackageVersionId);
+  }
+  public async getDependencies(subscriberPackageVersionId: string): Promise<Package2Version[]> {
+    const result: Package2Version[] = [];
+    if(!this.packageVersionIdToDependenciesIds.has(subscriberPackageVersionId))
+      this.packageVersionIdToDependenciesIds.set(subscriberPackageVersionId, await this.dxPackageMetadataApi.getDependenciesIds(subscriberPackageVersionId));
+    const ids = this.packageVersionIdToDependenciesIds.get(subscriberPackageVersionId);
+    if (ids.length === 0) return result;
+    const missingPackageIds: string[] = [];
+    ids.forEach(id => {
+        if (!this.packageVersions.has(id)) missingPackageIds.push('\'' + id + '\'');
+        else result.push(this.packageVersions.get(id));
+    });
+    if (missingPackageIds.length === 0) return result;
+    const packageVersions = await this.dxPackageMetadataApi.getPackage2VersionByIds(missingPackageIds);
+    packageVersions.forEach(packageVersion => {
+      result.push(packageVersion);
+      this.packageVersions.set(packageVersion.SubscriberPackageVersionId, packageVersion);
+    });
+    return result;
+  }
+}
